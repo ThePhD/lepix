868dc820b26d7800363289f72d85d3a255ca9227
ThePhD <phdofthehouse@gmail.com>
2016-11-28 07:40:09 -0500

    assume yes for ALL cases...

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -1,8 +1,8 @@
 apt-get update
 apt install -y git python python3 build-essential ocaml opam llvm llvm-dev llvm.3.8
 opam init
-opam install llvm
-opam install llvm.3.8
+opam install -y llvm
+opam install -y llvm.3.8
 eval $(opam config env)
 cd source
 make lepix
