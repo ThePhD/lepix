d6b090f44e6c316b258c622cd60dc7b1fab80e51
ThePhD <phdofthehouse@gmail.com>
2016-11-28 07:12:38 -0500

    poke at env to understand what's going on

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -1,11 +1,11 @@
 apt install update
-apt install git python python3 build-essential ocaml opam llvm llvm-dev llvm.3.8 
-opam init 
-opam install llvm 
-opam install llvm.3.8 
-eval $(opam config env) 
-cd source 
-make lepix 
-cd .. 
-python tests/tests.py 
+apt install git python python3 build-essential ocaml opam llvm llvm-dev llvm.3.8
+opam init
+opam install llvm
+opam install llvm.3.8
+eval $(opam config env)
+cd source
+make lepix
+cd ..
+python tests/tests.py
 cd ..
