871197ca963c38a588282f3bffbbf9baad562d76
ThePhD <phdofthehouse@gmail.com>
2016-11-28 14:10:00 -0500

    travis gooo

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -1,6 +1,7 @@
 apt-get update
 apt install -y git python python3 build-essential ocaml opam llvm llvm-dev llvm.3.8
-opam init
+opam init -y
+eval $(opam config env)
 opam install -y llvm
 opam install -y llvm.3.8
 eval $(opam config env)
