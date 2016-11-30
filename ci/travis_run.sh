b55ae1cdc2f5c8031a4cb17be7c617c7622716bf
ThePhD <phdofthehouse@gmail.com>
2016-11-30 07:54:26 -0500

    default llvm fails because Opam is a heaping pile of shit

--- ci/travis_run.sh
+++ ci/travis_run.sh
@@ -2,7 +2,6 @@ apt-get update
 apt install -y git python python3 build-essential m4 autotools-dev autoconf pkg-config ocaml menhir opam llvm llvm-dev llvm.3.8
 opam init -y
 eval $(opam config env)
-opam install -y llvm
 opam install -y llvm.3.8
 eval $(opam config env)
 cd source
